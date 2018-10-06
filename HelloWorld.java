e06a3a4eb50618518b59694f7ce5045dc2b25007
charliesmith381 <charles.smith@smes.org>
2018-10-06 18:39:34 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *  Name:    Charlie Smith
+ *  NetID:   cms11
+ *  Precept: P01B
+ *
+ *  Partner Name:    N/A
+ *  Partner NetID:   N/A
+ *  Partner Precept: N/A
+ * 
+ *  Description:  Prints 'Hello, World' to the terminal window.
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, World");
+    }
+}
